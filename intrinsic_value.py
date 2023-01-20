9b5bd9a75887328564460d32e46d30934f12fee8
Allen Nghayoui <allen.nghayoui@gmail.com>
2023-01-20 16:22:06 +0200

    bug fix

--- intrinsic_value.py
+++ intrinsic_value.py
@@ -2,12 +2,10 @@ import streamlit as st
 import requests
 import json
 
-from secret import call_api
-
 
 
 def get_comapany_data(symbol):
-    api_url = call_api(f'https://financialmodelingprep.com/api/v3/discounted-cash-flow/{symbol}?apikey=%s' % st.secrets['api_key'])
+    api_url = f'https://financialmodelingprep.com/api/v3/discounted-cash-flow/{symbol}?apikey=%s' % st.secrets['api_key']
     response = requests.get(api_url)
     return json.loads(response.text)[0]
 
